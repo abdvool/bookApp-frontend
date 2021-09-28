659f6b9f3d8bb4095e83a68f37a3065ec973ef60
abdvool <abdvool@gmail.com>
2021-09-28 16:31:38 +0300

    lab13

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,19 +1,71 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Jumbotron from 'react-bootstrap/Jumbotron';
+import Card from 'react-bootstrap/Card';
 import './BestBooks.css';
+import axios from 'axios';
+import { withAuth0 } from '@auth0/auth0-react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 class MyFavoriteBooks extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      Books: [],
+    }
+  }
+
+  componentDidMount = async () => {
+    console.log('inside componentDidMount')
+
+    let email1 = this.props.auth0.user.email
+    let BooksData = await axios.get(`${process.env.REACT_APP_SERVER}/getbooksOwner?email=${email1}`)
+
+    console.log(('wewew'),BooksData.data);
+    this.setState({
+      Books: BooksData.data
+
+    })
+    console.log(email1);
+    console.log('aaaaaaaaaa', this.state.books)
+  }
+
   render() {
-    return(
-      <Jumbotron>
-        <h1>My Favorite Books</h1>
-        <p>
-          This is a collection of my favorite books
-        </p>
-      </Jumbotron>
+    return (
+
+  <>
+
+        <Jumbotron>
+
+          <h1>My Favorite Books</h1>
+
+          <p>
+            This is a collection of my favorite books
+          </p>
+
+        </Jumbotron>
+
+        {this.state.Books.length > 0 && this.state.Books.map((ele,index) => {
+
+          return (
+
+            <Card key={index} style={{ width: '18rem' }}>
+
+              <Card.Body>
+                <Card.Title>{ele.title} </Card.Title>
+                <Card.Text> {ele.description}  </Card.Text>
+                <Card.Text>{ele.email}</Card.Text>
+
+              </Card.Body>
+
+            </Card>
+          )
+        })
+        }
+       </>
+
     )
   }
 }
 
-export default MyFavoriteBooks;
+export default withAuth0(MyFavoriteBooks);
---
+++ src/FindBook.js
@@ -0,0 +1,61 @@
+// import axios from 'axios';
+// import React from 'react'
+// import { withAuth0 } from '@auth0/auth0-react';
+
+// class App extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       Books: []
+//     }
+//   }
+
+//   componentDidMount = async ()=>{
+//     console.log('inside componentDidMount')
+
+//     let email1 = this.props.auth0.user.email
+//     let booksData = await axios.get(`${process.env.REACT_APP_SERVER}/getbooksOwner?email=${email1}`)
+//     this.setState({
+//         books: booksData.data
+
+//     })
+//     console.log(email1);
+//     console.log('aaaaaaaaaa',this.state.books)
+//   }
+
+// //   getCats = async (e) => {
+// //     e.preventDefault();
+
+// //     let ownerName = e.target.catOwner.value;
+// //     console.log(ownerName);
+
+// //     // localhost:3001/getbooksOwner?ownerName=razan
+// //         let booksData = await axios.get(`${process.env.REACT_APP_SERVER}/getbooksOwner?email=${email}`)
+// //     this.setState({
+// //       books: booksData.data
+// //     })
+// //     console.log(this.state.books)
+
+// //   }
+
+//   render() {
+//     return (
+//       <div>
+//         {/* <form onSubmit={this.getbooks}>
+//           <label>Enter your name</label>
+//           <input type="text" name='catOwner' />
+//           <input type="submit" value="get books" />
+//         </form> */}
+//         {/* render the books data */}
+//         {/* {this.state.books.map
+//         <Cat/>} */}
+//       </div>
+//     )
+//   }
+// }
+
+
+
+
+// export default withAuth0(App);
